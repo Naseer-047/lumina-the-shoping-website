5fbf1b635145e118c05e35eaf04ac4d4d816523a
Naseer Pasha <n763122562@gmail.com>
2026-01-20 19:04:02 +0530

    --------

--- src/utils/formatting.js
+++ src/utils/formatting.js
@@ -1,8 +1,4 @@
-/**
- * Formats a number as Indian Rupee
- * @param {number} amount 
- * @returns {string} e.g., "₹1,200"
- */
+
 export const formatCurrency = (amount) => {
     return new Intl.NumberFormat('en-IN', {
         style: 'currency',
